bec79e94d5a7b1afbf8d90926d5bb37af8296f5f
EdwarCastellanos5120 <ecastellanosp1@miumg.edu.gt>
2023-08-29 19:07:33 -0600

    Modificaciones a codigo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese una palabra o frase para verificar si es palindromo");
         String cadena = scanner.nextLine();
         Palindromos palindromos = new Palindromos();
         if (palindromos.verificarPalabra(cadena)) {
